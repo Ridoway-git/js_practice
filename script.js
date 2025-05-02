15875bdb5709b27cd35fc84692718d54b5fe3d99
Ridoway <loops.ridoway@gmail.com>
2025-05-03 03:43:59 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function myFunction() {
+    document.getElementById('fn').innerHTML="paragraph changed";
+
+}
+
+document.getElementById('helloh2').innerHTML = "<h2>Hello World</h2>";
+// document.write(3+3);
+// window.alert(4+3);
+console.log(44+33);
+let x,y,z;
+x=3;
+y=4;
+z= x+y;
+// x=3; y=4; z=4;one line allowed
+document.getElementById('statement').innerHTML = "The value of z is "+ z + ".";
+function anotherFunction() {
+    document.getElementById('f1').innerHTML = "hello js";
+    document.getElementById('f2').innerHTML = "how are you ridoy";
+
+}
+
+document.getElementById('nums').innerHTML = 10.30;
+document.getElementById('str').innerHTML = "ridoy"+ ' Sidoy';
+let d;
+d = 33;
+document.getElementById('x1').innerHTML= d;
+document.getElementById('op').innerHTML =(5+4)*3;
+document.getElementById('name').innerHTML = "john" + " " + "doe";
+let c;
+// c= 33; this is comment
+c= 3;//i will be executed
+
+// A JavaScript name must begin with:
+
+// A letter (A-Z or a-z)
+// A dollar sign ($)
+// Or an underscore (_)
+
+
