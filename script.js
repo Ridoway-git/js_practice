9c0adc709436ba5ede049b75b2348157d3c5add9
Ridoway <loops.ridoway@gmail.com>
2025-05-04 17:13:01 +0600

    third commit

--- script.js
+++ script.js
@@ -36,9 +36,41 @@ c= 3;//i will be executed
 // A dollar sign ($)
 // Or an underscore (_)
 
+var mn=22;
+var xr = 33;
+var ss = mn+xr;
+document.getElementById('var').innerHTML = "the value of z is " + ss;
 
 // this is single line comment 
 /*this is multiline comment*/
 
 document.getElementById('myH').innerHTML ="this is heading";
 document.getElementById('p1').innerHTML ="this is paragraph";
+
+const pi = 3.14;
+let persons = "ridoy khan"
+let ages = 24;
+document.getElementById('yiyi').innerHTML= pi  + "<br>"+ persons + "<br>" + ages;
+let pName = "ridoy", carName = "volvo" , price = 300;
+document.getElementById('car').innerHTML=  price;
+
+let carnames;
+document.getElementById('undefined').innerHTML= carnames;   //output undefined
+
+let xx= "44" + 4 +3;
+document.getElementById('aa').innerHTML = xx;
+let yy= 5+5+"10";
+document.getElementById('aaa').innerHTML = yy;
+let $ = "hello world";
+let $$$ = 2;
+let $myMoney = 5;
+document.getElementById('toto').innerHTML = $ + " and my money is " + $$$ + $myMoney + "and i am very happy";
+
+let ina  = 44;
+ina+= 3;
+document.getElementById("plus").innerHTML = ina;
+let text1 = "A";
+let text2 = "B";
+let result = text1 > text2;
+document.getElementById("t").innerHTML = "is a less than B " + result;
+
