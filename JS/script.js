440ec706eb3690c46ae704211a92039f5730345d
Your Name <your.email@example.com>
2025-05-09 23:41:52 +0600

    another todaycommit

---
+++ JS/script.js
@@ -0,0 +1,60 @@
+// let x = 0.2 + 0.4 ;
+// let y = (0.2*10 + 0.4*10 ) /10;
+// console.log(x);
+
+// console.log(y);
+
+// let x = new Number (400);
+// let y = new Number (400);
+
+// console.log(x == y); 
+// console.log(typeof x + typeof y);
+
+// let x = 12345678901234567890135n;
+// let y = BigInt("123456789012345678901234567890");
+
+// console.log(typeof y);
+
+// let x =  123;
+// console.log(x.toString() + "<br>" + (123).toString() + "<br>" + (100+23).toString());
+
+
+// const cars = ["saab", "volvo", "bmw"];
+// console.log(cars);
+
+// const cars = [
+//     "saab",
+//     "volvo",
+//     "bmw"
+// ];
+
+// console.log(cars);
+
+// const cars = [] ;
+// cars[0] = "saab";
+// cars[1] = "volvo";
+// cars [2] ="bmw";
+// console.log(cars);
+
+// const cars = new Array ("saab", "volvo", "bmw");
+// console.log(cars);
+
+// const cars= ["saab", "volvo", "bmw"];
+// console.log(cars[0]);
+
+// const fruits = ["apple", "mango", "orange"];
+// console.log(fruits.toString());
+
+// const fruits= ["apple", "orange", "mango"];
+// console.log(fruits.length);
+
+// const names = ["ridoy", "khan", "mominul"];
+// console.log(names[names.length -1]);
+
+
+// const  fruits = ["mango", "apple", "guava"];
+
+// fruits.push("lemon");
+// console.log(fruits);
+
+
