780033281a3021aa8ddb1a7d04f22d32d82f6fd6
Your Name <your.email@example.com>
2025-05-10 00:58:23 +0600

    a commit

--- JS/script.js
+++ JS/script.js
@@ -57,4 +57,93 @@
 // fruits.push("lemon");
 // console.log(fruits);
 
+// const blue = ["banana", "blue", "black"];
+// blue.length = "lemon";
+// console.log(blue);
+
+// const fruits = ["banana", "orange", "apple"];
+// fruits[6] = "lemon";
+
+// console.log(fruits);
+
+// const person =[];
+// person["firstName"] = "john";
+// person["lastName"] = "Doe";
+// person["age"] = 23;
+// console.log(person[0] + " " + person.length);
+
+// const points =[30,233,1,4,23];
+// console.log(points[0]);
+
+// var points = new Array(39,33,32);
+// console.log(typeof points);
+
+// const lol = ["ridoy","mominul","khan"];
+// console.log(Array.isArray(lol));
+
+// let x= "";
+// const myObj = {
+//     name: "john",   
+//     age : 39,
+//     cars : [
+//         {
+//             name:"ford", models:["fiesta", "focus", "mustang"]
+//         },
+//         {
+//             name: "bmw", models:["320","x3","x5"]
+//         },
+//         {
+//             name: "flat", models: ["400", "panda"]
+//         }
+//     ]
+// }
+
+// for (let i in myObj.cars) {
+//     x += "<h2>" + myObj.cars[i].name + "</h2>";
+//     for (let j in myObj.cars[i].models) {
+//         x+= myObj.cars[i].models[j] + "<br>";
+//     }
+// }
+
+// console.log(x);
+
+
+// const fruits = ["ridoy", "khan", "orange"];
+// console.log(fruits.toString());
+
+// const fruits = ["wow", "khan", "orange", "mango"];
+// console.log(fruits.at(2));
+
+// const fruits = ["banana", "apple", "orange" , "mango"];
+// console.log(fruits.join(" * "));
+
+// const fruits = ["banana", "apple", "mango"];
+// fruits.pop();
+// console.log(fruits);
+
+// const wow = ["banana", "apple", "orange"];
+// wow.shift();
+// console.log(wow);
+
+// const wwow = ["lol", "khan", "blue"];
+// wwow.unshift("ridoy");
+// console.log(wwow);
+
+// const aaa = ["aa","bb","ccc"];
+// aaa[0] = "kiwi";
+// console.log(aaa);
+
+// const myGirls = ["cecilie", "lone"];
+// const myBoys = ["email", "tobias", "linus"];
+// const myChildren = myGirls.concat(myBoys);
+
+// console.log(myChildren);
+
+// const array1 = ["celilie", "lone"];
+// const array2 = ["emil", "tobias"];
+// const array3 = ["robin", "morgan"];
+
+// const myChildren = array1.concat(array2,array3);
+
+// console.log(myChildren);
 
